7b913973511c9452f7d0162131769de976550a82
webdiziner01 <webdiziner01@gmail.com>
2018-07-17 14:03:21 +0500

    first commit on video 08

---
+++ app/Area.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
+class Area extends Model
+{
+    use NodeTrait;
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Area;
+class HomeController extends Controller
+{
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $areas = Area::get()->toTree();
+
+        return view('home', compact('areas'));
+    }
+}
